f0b6362178d9fc66fb20cd71fe407ce5f2bf3701
Artsiom Azarenka <a.azarenka@godeltech.com>
2019-03-18 11:14:34 +0300

    Split funcs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -29,18 +29,15 @@ func RequestLogger(targetMux http.Handler) http.Handler {
 
 func main() {
 	fileName := "webrequests.log"
-
 	logFile, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
 
 	if err != nil {
 		panic(err)
 	}
-
 	defer logFile.Close()
 
-	// direct all log messages to webrequests.log
-	//log.SetOutput(logFile)
-	log.SetOutput(os.Stdout)
+	log.SetOutput(logFile)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/tops", tops.HandlerTops)
 	//http.HandleFunc("/tops", tops.HandlerTops)
