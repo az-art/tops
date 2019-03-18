b253d7fa470e6df9978d494ee7fe723f1e3da850
Artsiom Azarenka <a.azarenka@godeltech.com>
2019-03-18 11:00:01 +0300

    Split funcs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,9 +4,43 @@ import (
 	"github.com/az-art/tops/pkg/tops"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
+func RequestLogger(targetMux http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		targetMux.ServeHTTP(w, r)
+
+		// log request by who(IP address)
+		requesterIP := r.RemoteAddr
+
+		log.Printf(
+			"%s\t\t%s\t\t%s\t\t%v",
+			r.Method,
+			r.RequestURI,
+			requesterIP,
+			time.Since(start),
+		)
+	})
+}
+
 func main() {
+	fileName := "webrequests.log"
+
+	logFile, err := os.OpenFile(fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer logFile.Close()
+
+	// direct all log messages to webrequests.log
+	log.SetOutput(logFile)
+
 	http.HandleFunc("/tops", tops.HandlerTops)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
