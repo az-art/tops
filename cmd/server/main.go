6c31ad4038d3e02e732e25c0aaa7df7115e5f2c2
Artsiom Azarenka <a.azarenka@godeltech.com>
2019-03-18 12:04:59 +0300

    add Dockerfile

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,12 +1,35 @@
 package main
 
 import (
+	"context"
 	"github.com/az-art/tops/pkg/tops"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
+
+func main() {
+	log.SetOutput(os.Stdout)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/tops", tops.HandlerTops)
+	log.Println("Starting Server. Listening on port 8000")
+
+	srv := &http.Server{
+		Handler:      RequestLogger(mux),
+		Addr:         ":8000",
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+	//log.Fatal(http.ListenAndServe("localhost:8000", RequestLogger(mux)))
+
+	waitForShutdown(srv)
+}
 
 func RequestLogger(targetMux http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -27,10 +50,18 @@ func RequestLogger(targetMux http.Handler) http.Handler {
 	})
 }
 
-func main() {
-	log.SetOutput(os.Stdout)
-	mux := http.NewServeMux()
-	mux.HandleFunc("/tops", tops.HandlerTops)
-	log.Printf("Starting server on port 8000\n")
-	log.Fatal(http.ListenAndServe("localhost:8000", RequestLogger(mux)))
+func waitForShutdown(srv *http.Server) {
+	interruptChan := make(chan os.Signal, 1)
+	signal.Notify(interruptChan, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	// Block until we receive our signal.
+	<-interruptChan
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+	srv.Shutdown(ctx)
+
+	log.Println("Shutting down")
+	os.Exit(0)
 }
