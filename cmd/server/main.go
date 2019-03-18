3c4cb5ad65e65433bd0ac78c24d9bde1e516c3b0
Artsiom Azarenka <a.azarenka@godeltech.com>
2019-03-18 10:32:47 +0300

    Split funcs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,36 +7,8 @@ import (
 	"os/exec"
 )
 
-type Command struct {
-	com  string
-	args []string
-	w    http.ResponseWriter
-	res  result
-}
-
-type result struct {
-	out []byte
-	err error
-}
-
 func main() {
 	var c Command
 	http.HandleFunc("/top", c.executeTop)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
-
-func (c *Command) executeTop(w http.ResponseWriter, req *http.Request) {
-	c.com = "top"
-	c.args = []string{"-b", "-n", "1"}
-	c.w = w
-	c.exec()
-}
-
-func (c *Command) exec() {
-	cmd := exec.Command(c.com, c.args...)
-	if c.res.out, c.res.err = cmd.Output(); c.res.err != nil {
-		println(c.res.err.Error())
-	}
-	fmt.Fprint(c.w, string(c.res.out))
-}
-
---
+++ pkg/tops/tops.go
@@ -0,0 +1,34 @@
+package tops
+
+import (
+	"fmt"
+	"net/http"
+	"os/exec"
+)
+
+type Command struct {
+	com  string
+	args []string
+	w    http.ResponseWriter
+	res  result
+}
+
+type result struct {
+	out []byte
+	err error
+}
+
+func (c *Command) HandlerTops(w http.ResponseWriter, req *http.Request) {
+	c.com = "top"
+	c.args = []string{"-b", "-n", "1"}
+	c.w = w
+	c.exec()
+}
+
+func (c *Command) exec() {
+	cmd := exec.Command(c.com, c.args...)
+	if c.res.out, c.res.err = cmd.Output(); c.res.err != nil {
+		println(c.res.err.Error())
+	}
+	fmt.Fprint(c.w, string(c.res.out))
+}
